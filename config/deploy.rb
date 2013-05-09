2921b2fba23a6c9741b779aa351d43b50be99f58
James Smith <james@floppy.org.uk>
2013-05-09 13:08:08 +0100

    add keycard file setup to deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ role :web, "signin.office.theodi.org"
 role :app, "signin.office.theodi.org"
 role :db,  "signin.office.theodi.org", :primary => true
 
-after "deploy:update_code", "deploy:link_config"
+after "deploy:update_code", "deploy:setup_card_file", "deploy:link_config"
 
 namespace :deploy do
   task :link_config do
@@ -20,6 +20,11 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/data/stock #{release_path}/staff/stock"
     run "ln -nfs #{shared_path}/data/staff.csv #{release_path}/staff.csv"
     run "ln -nfs #{shared_path}/data/stock #{release_path}/stock"
+    run "ln -nfs #{shared_path}/keycard.txt #{release_path}/keycard.txt"
+  end  
+  task :setup_card_file do
+    run "touch #{shared_path}/keycard.txt"
+    run "chmod 777 #{shared_path}/keycard.txt"
   end  
 end
 
